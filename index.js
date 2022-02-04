c92a67d5787884e3e89b61d03adbf948d599efef
Tammy <tammycollinse@gmail.com>
2022-02-03 21:10:01 -0500

    commit tree 38dc6a0440cd00a07374b587def9b8a3e7dedec6

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+//document.addEventListener("keydown", function (event) {
+  //  if (event.key === "ArrowLeft") {
+    //  const leftNumbers = dodger.style.left.replace("px", "");
+     // const left = parseInt(leftNumbers, 10);
+  
+    //  dodger.style.left = `${left - 1}px`;
+    //}
+  //});
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+      
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener('keydown', function(e){
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight(){
+    
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+      
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  document.addEventListener('keydown', function(e){
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
+  
